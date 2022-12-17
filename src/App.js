3aabdc5fc933aa8f0298fd204a389527d3025696
Ajmal junaid <ajmal6201@gmail.com>
2022-12-17 18:43:29 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import './App.css'
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+import {action,comedy,documentaries,horror,originals, romance} from './urls'
+function App() {
+  return (
+     <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={comedy} title='Comedy-Movies' isSmall/>
+      <RowPost url={horror} title='Horror-Movies' isSmall/>
+      <RowPost url={romance} title='Romantic-Movies' isSmall/>
+      <RowPost url={documentaries} title='Documentaries' isSmall/>
+      <RowPost url={action} title='Action' isSmall/>
+     </div>
+  );
+}
+
+export default App;
